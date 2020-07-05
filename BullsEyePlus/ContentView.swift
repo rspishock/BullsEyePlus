83d5ac8fb2b7f620ab52915f3e20ec02d693327d
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-07-05 15:16:27 -0400

    Update UI for AppleWatch8

--- BullsEyePlus/ContentView.swift
+++ BullsEyePlus/ContentView.swift
@@ -30,51 +30,56 @@ import SwiftUI
 import Game
 
 struct ContentView: View {
-  @ObservedObject private var game = BullsEyeGame()
-
-  @State private var currentValue = 50.0
-  @State private var showAlert = false
-
-  private var alpha: Double {
-    abs(Double(game.targetValue) - currentValue) / 100.0
-  }
-
-  var body: some View {
-    VStack {
-      Text("Put the Bull's Eye as close as you can to: \(game.targetValue)")
-      HStack {
-        Text("0")
-        Slider(value: $currentValue, in: 1.0...100.0, step: 1.0)
-          .background(Color.blue)
-          .opacity(alpha)
-        Text("100")
-      }
-      .padding(.horizontal)
-      Button(action: {
-        self.showAlert = true
-        self.game.checkGuess(Int(self.currentValue))
-      }) {
-        Text("Hit Me!")
-      }.alert(isPresented: $showAlert) {
-        Alert(title: Text("Your Score"), message: Text(String(game.scoreRound)),
-              dismissButton: .default(Text("OK"), action: {
-                self.game.startNewRound()
-                self.currentValue = 50.0
-              }))
-      }
-      .padding()
-      HStack {
-        Text("Total Score: \(game.scoreTotal)")
-        Text("Round: \(game.round)")
-      }
+    @ObservedObject private var game = BullsEyeGame()
+    @EnvironmentObject var defaults: UserDefaults
+    
+    @State private var currentValue = 50.0
+    @State private var showAlert = false
+    
+    private var alpha: Double {
+        abs(Double(game.targetValue) - currentValue) / 100.0
     }
-  }
+    
+    var body: some View {
+        VStack {
+            Text("Put the Bull's Eye as close as you can to: \(game.targetValue)")
+            HStack {
+                Text("0")
+                if defaults.bool(forKey: "show_hint") {
+                    Slider(value: $currentValue, in: 1.0...100.0, step: 1.0)
+                        .background(Color.blue)
+                        .opacity(abs((Double(self.game.targetValue) - self.currentValue)/100.0))
+                } else {
+                    Slider(value: $currentValue, in: 1.0...100.0, step: 1.0)
+                }
+                Text("100")
+            }
+            .padding(.horizontal)
+            Button(action: {
+                self.showAlert = true
+                self.game.checkGuess(Int(self.currentValue))
+            }) {
+                Text("Hit Me!")
+            }.alert(isPresented: $showAlert) {
+                Alert(title: Text("Your Score"), message: Text(String(game.scoreRound)),
+                      dismissButton: .default(Text("OK"), action: {
+                        self.game.startNewRound()
+                        self.currentValue = 50.0
+                      }))
+            }
+            .padding()
+            HStack {
+                Text("Total Score: \(game.scoreTotal)")
+                Text("Round: \(game.round)")
+            }
+        }
+    }
 }
 
 struct ContentView_Previews : PreviewProvider {
-  static var previews: some View {
-    ContentView()
-    //      .previewLayout(.fixed(width: 568, height: 320))
-  }
+    static var previews: some View {
+        ContentView().environmentObject(UserDefaults.standard)
+        //      .previewLayout(.fixed(width: 568, height: 320))
+    }
 }
 
